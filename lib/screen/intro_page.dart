93e5b2017cd73434a30aad3bcd2897c9e66b85fd
ikhela-04KK <kankoffi36@gmail.com>
2024-04-10 07:52:28 +0200

    Hiérachisez la structure de mes dossiers et fichiers

--- lib/screen/intro_page.dart
+++ lib/screen/intro_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: body_might_complete_normally_nullable
+
 import 'package:animate_do/animate_do.dart';
 import 'package:crypto_wallet_app/screen/home_page.dart';
 import 'package:flutter/material.dart';
